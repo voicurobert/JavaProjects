7028cd05d275f6e5b529cee4aad2c0a2bb2f08bb
robert <voicu.eduardrobert@gmail.com>
2020-02-06 20:49:02 +0200

    course 8

--- jpa_courses/jpac8/src/main/java/main/java/entities/DepartmentDetails.java
+++ jpa_courses/jpac8/src/main/java/main/java/entities/DepartmentDetails.java
@@ -3,35 +3,32 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package main.java.entities;
-
 
 import javax.persistence.Embeddable;
 import javax.persistence.ManyToOne;
-
 
 @Embeddable
 public class DepartmentDetails {
 
     private String contractNo;
-    
+
     @ManyToOne
     private Department department;
-    
+
     public String getContractNo() {
-		return contractNo;
-	}
-    
+        return contractNo;
+    }
+
     public Department getDepartment() {
-		return department;
-	}
-    
+        return department;
+    }
+
     public void setContractNo(String contractNo) {
-		this.contractNo = contractNo;
-	}
-    
+        this.contractNo = contractNo;
+    }
+
     public void setDepartment(Department department) {
-		this.department = department;
-	}
+        this.department = department;
+    }
 }
