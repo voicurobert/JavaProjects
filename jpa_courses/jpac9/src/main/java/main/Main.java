b9b55e0055f1342334f57a39f182ff390b050034
robert <voicu.eduardrobert@gmail.com>
2020-02-06 21:46:30 +0200

    course 9

---
+++ jpa_courses/jpac9/src/main/java/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-persistence-unit");
+        EntityManager em = emf.createEntityManager();
+    }
+}
