2c290d7865c97aaf1df98dc4c4881ca12891ab3a
robert <voicu.eduardrobert@gmail.com>
2020-02-15 15:19:28 +0200

    course 11

--- jpa_courses/jpac11_entityManager/src/main/java/main/Main.java
+++ jpa_courses/jpac11_entityManager/src/main/java/main/Main.java
@@ -2,6 +2,7 @@ package main;
 
 
 import main.entities.Product;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -10,26 +11,29 @@ import javax.persistence.Persistence;
 public class Main {
 
     /*
-    persist()   - add instances to the context
+    persist()               - add instances to the context
 
-    flush() - forces the implementation to make the changes from the entity manager context before the commit
+    flush()                 - forces the implementation to make the changes from the entity manager context before the commit
 
-    find()  - retrieve an instance from the database using its id
-            - null will be returned if the object does not exits in the database
+    find()                  - retrieve an instance from the database using its id
+                            - null will be returned if the object does not exits in the database
 
-    getReference()  - does not retrieve all the details of the instances
+    getReference()          - does not retrieve all the details of the instances
+                            - is lazy. Only gives a proxy it won t trigger the select in the database until the instance is used!!!
 
-    contains()  - instance exists in the context
+    contains()              - instance exists in the context
 
-    detach()    - removes instance from context
+    detach()                - removes instance from context
 
-    clear() - removes all the instances from context
+    clear()                 - removes all the instances from context
 
-    remove()    -
+    remove()                - marks the instance to be deleted, removes it from context and from the database
+                            - works only on managed instances, it has to be in a context
 
-    merge() -
+    merge()                 - add an instance in the context, it must exists at database level
 
-    refresh()   -
+    refresh()               - will undo all the changes for a specific instance
+                            - updates the instance with what we have in the database
 
      */
 
@@ -38,7 +42,7 @@ public class Main {
         EntityManager em = emf.createEntityManager();
 
         em.getTransaction().begin();
-//
+
 //        Product p = new Product();
 //        p.setId(11);
 //        p.setName("P1");
@@ -47,9 +51,29 @@ public class Main {
 //        em.flush();
 //
 //        System.out.println(":)");
+//         Product p = em.find(Product.class, 10);
+//         System.out.println(em.contains(p));
 
-        Product p = em.find(Product.class, 10);
 
+//        Product p = em.find(Product.class, 11);
+//        em.remove(p);
+
+//        Product p = new Product();
+//        p.setId(11);
+//        em.merge(p);
+//        System.out.println(em.contains(p));
+//        em.remove(p);
+
+//        Product p = em.find(Product.class, 11);
+//        p.setName("A");
+//        em.refresh(p);
+
+        Product p = new Product();
+        p.setName("BLA");
+        p.setId(20);
+        em.persist(p);
+        em.detach(p); // takes p out from the context
+        em.clear();   // takes all instances out of the context
 
 
         em.getTransaction().commit();
--- jpa_courses/jpac11_entityManager/src/main/java/main/entities/Product.java
+++ jpa_courses/jpac11_entityManager/src/main/java/main/entities/Product.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Objects;
 
 @Entity
 public class Product {
@@ -27,5 +28,18 @@ public class Product {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return id == product.id && Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
     }
 }
