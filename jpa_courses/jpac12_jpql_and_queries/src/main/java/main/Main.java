8ca67cf38bbebffcdb224028a80f8eb3e55970ca
robert <voicu.eduardrobert@gmail.com>
2020-02-15 16:45:56 +0200

    course 12

--- jpa_courses/jpac12_jpql_and_queries/src/main/java/main/Main.java
+++ jpa_courses/jpac12_jpql_and_queries/src/main/java/main/Main.java
@@ -1,17 +1,23 @@
 package main;
 
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import main.entities.Employee;
+import main.entities.Product;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.stream.Stream;
 
 
 public class Main {
 
     /*
+    SQL92
 
-        JPQL 
 
+    JPQL is platform independent
+    JPQL    - sql select like
+            - SELECT p FROM Product p                   Product is the table class in jpa
      */
 
     public static void main(String[] args) {
@@ -20,7 +26,38 @@ public class Main {
 
         em.getTransaction().begin();
 
+//        String jpql = "SELECT p FROM Product p";
+//        TypedQuery<Product> q = em.createQuery(jpql, Product.class);
+//        String jpql = "SELECT p FROM Product p WHERE p.price > :price"; // :price is a jpql parameter
+//        TypedQuery<Product> q = em.createQuery(jpql, Product.class);
+//        q.setParameter("price", 10.0);
 
+//        String jpql = "SELECT SUM(p.price) FROM Product p WHERE p.price > :price";
+//        TypedQuery<Double> q = em.createQuery(jpql, Double.class);
+//        q.setParameter("price", 10.0);
+//        double d = q.getSingleResult();
+//        System.out.println(d);
+
+        // named query, are validated on application startup
+
+//        TypedQuery<Product> q = em.createNamedQuery("Product.all", Product.class);
+//        q.getResultStream().forEach(System.out::println);
+//
+//        String jpql = "SELECT SUM(p.price) FROM Product p WHERE p.price > ?1";
+//        TypedQuery<Double> q2 = em.createQuery(jpql, Double.class);
+//        q2.setParameter(1, 10.0);
+//
+//        // native queries
+//        String sql = "SELECT * FROM product p";
+//        Query query = em.createNativeQuery(sql);
+//        query.getResultList().forEach(System.out::println);
+
+
+        String jpql = "SELECT e FROM Employee e, Department d WHERE e.department = d AND d.id = :id";
+        TypedQuery<Employee> query = em.createQuery(jpql, Employee.class);
+        query.setParameter("id", 1);
+        List<Employee> list = query.getResultList();
+        list.forEach(System.out::println);
 
         em.getTransaction().commit();
         em.close();
---
+++ jpa_courses/jpac12_jpql_and_queries/src/main/java/main/entities/Department.java
@@ -0,0 +1,51 @@
+package main.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@Entity
+public class Department {
+
+    @Id
+    private int id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "department")
+    private List<Employee> employees;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", employees=" + employees +
+                '}';
+    }
+}
