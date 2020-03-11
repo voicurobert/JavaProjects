07fd2f6bfa11d5509166d80f8b80bf2f8d2fcc04
robert <voicu.eduardrobert@gmail.com>
2020-03-11 09:27:46 +0200

    course 08

---
+++ spring_course/course_08_spring_boot_101/src/main/java/com/rvo/course_08_spring_boot_101/Course08SpringBoot101Application.java
@@ -0,0 +1,13 @@
+package com.rvo.course_08_spring_boot_101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course08SpringBoot101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Course08SpringBoot101Application.class, args);
+	}
+
+}
---
+++ spring_course/course_08_spring_boot_101/src/test/java/com/rvo/course_08_spring_boot_101/Course08SpringBoot101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rvo.course_08_spring_boot_101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course08SpringBoot101ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
